74da1326e1dd94535ae3079902594733a1056fbb
Artur Voloshin <nozeilwrk@gmail.com>
2024-05-16 14:19:32 +0300

    feat: implement proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+const source = '/themoviedb-api/:path*';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  rewrites: async () => [{ source, destination: 'https://api.themoviedb.org/3/:path*' }],
 };
 
 export default nextConfig;
